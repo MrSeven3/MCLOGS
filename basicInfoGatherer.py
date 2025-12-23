8989c4066641d6b186f9b696bc4e737c7998dc5b
V3AI-Seven <127050193+MrSeven3@users.noreply.github.com>
2025-12-22 22:31:22 -0500

    Can now get neoforge loader version. MC version is much more annoying, as theres two places to check.

--- basicInfoGatherer.py
+++ basicInfoGatherer.py
@@ -1,22 +1,28 @@
 import re
 from io import TextIOWrapper
 
-NEOFORGE_LOADER_DETECT_REGEX = r".* \[main\/INFO\] (\[net\.neoforged\.fml\.loading\.moddiscovery\.ModDiscoverer/SCAN\]).*\s*"
+NEOFORGE_LOADER_DETECT_REGEX = r".*\[main\/INFO\] \[cpw\.mods\.modlauncher\.Launcher\/MODLAUNCHER\]: .* --fml\.neoForgeVersion"
+
+NEOFORGE_LOADER_VERSION_FLAG_EXTRACT_REGEX = r".*\[main\/INFO\] \[cpw\.mods\.modlauncher\.Launcher\/MODLAUNCHER\]: .* --fml\.neoForgeVersion, ([0-9]{2}\.[0-9]{1,2}\.[0-9]{1,3}), "
+NEOFORGE_REG_VERSION_FLAG_EXTRACT_REGEX = r".*\[main\/INFO\]: .* --version, ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|neoforge-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})" #gets the version flag. this is either the mc version or the loader version
 
 
-def checkLoader(file:TextIOWrapper) -> str:
+def checkGameData(file:TextIOWrapper) -> str:
     loader = ""
-    lines = []
+    loaderVer = ""
+    minecraftVer = ""
+    javaVer = ""
     for i, line in enumerate(file):
-        if i < 20:
-            #print("Loaded line " +str(i) +" with data "+ line)
-            lines.append(line)
-        else:
-            break
+        if i == 0 : #check first line for useful info
+            #check if neoforge loader
+            if re.match(NEOFORGE_LOADER_DETECT_REGEX,line):
+                loader = "Neoforge"
 
-    for line in lines:
-        if re.match(NEOFORGE_LOADER_DETECT_REGEX,line):
-            loader = "Neoforge"
-            break
+                loaderVer = re.findall(NEOFORGE_LOADER_VERSION_FLAG_EXTRACT_REGEX, line) #detect neoforge loader version
+                print("Detected loader is Neoforge, version " +str(loaderVer[0]))
 
-    return loader
+                versionFlagData = re.match(NEOFORGE_REG_VERSION_FLAG_EXTRACT_REGEX, line)
+                if versionFlagData[0]
+
+    gameData = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer,"javaVersion":javaVer}
+    return gameData
