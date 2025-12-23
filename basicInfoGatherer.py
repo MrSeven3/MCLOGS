b7a241eea4426c734dbe06c0517d45acf397e415
V3AI-Seven <127050193+MrSeven3@users.noreply.github.com>
2025-12-23 12:25:29 -0500

    got all of the neoforge basic data collection hopefully universal

--- basicInfoGatherer.py
+++ basicInfoGatherer.py
@@ -1,12 +1,10 @@
 import re
 from io import TextIOWrapper
 
-NEOFORGE_LOADER_DETECT_REGEX = r".*\[main\/INFO\] \[cpw\.mods\.modlauncher\.Launcher\/MODLAUNCHER\]: .* --fml\.neoForgeVersion"
+NEOFORGE_LOADER_DETECT_REGEX = r"\t{2}NeoForge.*"
 
-NEOFORGE_LOADER_VERSION_FLAG_EXTRACT_REGEX = r".*\[main\/INFO\] \[cpw\.mods\.modlauncher\.Launcher\/MODLAUNCHER\]: .* --fml\.neoForgeVersion, ([0-9]{2}\.[0-9]{1,2}\.[0-9]{1,3}), "
-NEOFORGE_MC_VERSION_EXTRACT_REGEX = r"([0-9]{1,2}\.[0-9]{1,2})\.[0-9]{1,4}" #extracts the minecraft version from the provided neoforge version
-NEOFORGE_JAVA_VERSION_LINE_DETECT_REGEX = r".*\[main\/INFO\].*: ModLauncher.+ java version "
-NEOFORGE_JAVA_VERSION_EXTRACT_REGEX= r".*\[main\/INFO\].*: ModLauncher.+ java version ([0-9]{1,2})\.[0-9]{1,3}\.[0-9]{1,3} by .*"
+NEOFORGE_LOADER_VERSION_EXTRACT_REGEX = r"\t{2}NeoForge ([0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,4}).*"
+NEOFORGE_MC_VERSION_EXTRACT_REGEX = r"([0-9]{1,2}\.[0-9]{1,3})\.[0-9]{1,4}" #extracts the minecraft version from the provided neoforge version
 
 def checkGameVersions(file:TextIOWrapper) -> str:
     loader = ""
@@ -14,25 +12,16 @@ def checkGameVersions(file:TextIOWrapper) -> str:
     minecraftVer = ""
     javaVer = ""
     for i, line in enumerate(file):
-        if i == 0 : #check first line for useful info
-            #check if neoforge loader
+        if i < 700:
             if re.match(NEOFORGE_LOADER_DETECT_REGEX,line):
-                loader = "Neoforge"
-
-                loaderVer = re.findall(NEOFORGE_LOADER_VERSION_FLAG_EXTRACT_REGEX, line)[0] #detect neoforge loader version
-                print("Detected loader is Neoforge, version " +str(loaderVer))
+                loader = "NeoForge"
+                loaderVer = re.findall(NEOFORGE_LOADER_VERSION_EXTRACT_REGEX,line)[0]
+                print("Detected loader is Neoforge, version " + str(loaderVer))
 
                 minecraftVer = re.findall(NEOFORGE_MC_VERSION_EXTRACT_REGEX,loaderVer)[0]
                 minecraftVer = "1." + minecraftVer
                 print("Minecraft version is " + str(minecraftVer))
 
-        if i < 5:
-            #run extra info gathering for neoforge
-            if loader == "Neoforge":
-                if re.match(NEOFORGE_JAVA_VERSION_LINE_DETECT_REGEX,line):
-                    javaVer = re.findall(NEOFORGE_JAVA_VERSION_EXTRACT_REGEX, line)[0]
-                    print("Running Major Java version " +str(javaVer))
 
-
-    gameVersions = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer,"javaVersion":javaVer}
+    gameVersions = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer}
     return gameVersions
