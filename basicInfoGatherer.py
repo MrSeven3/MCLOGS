f14bacd0c69bb9c9744de8b7c268d7b0d12001ad
V3AI-Seven <127050193+MrSeven3@users.noreply.github.com>
2025-12-23 12:45:20 -0500

    Fabric loader, loader version, and minecraft version detection

--- basicInfoGatherer.py
+++ basicInfoGatherer.py
@@ -6,22 +6,33 @@ NEOFORGE_LOADER_DETECT_REGEX = r"\t{2}NeoForge.*"
 NEOFORGE_LOADER_VERSION_EXTRACT_REGEX = r"\t{2}NeoForge ([0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,4}).*"
 NEOFORGE_MC_VERSION_EXTRACT_REGEX = r"([0-9]{1,2}\.[0-9]{1,3})\.[0-9]{1,4}" #extracts the minecraft version from the provided neoforge version
 
+
+FABRIC_LOADER_DETECT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+
+FABRIC_LOADER_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*"
+FABRIC_MC_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft ([0-9]\.[0-9]{1,2}\.[0-9]{1,2}) with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+
 def checkGameVersions(file:TextIOWrapper) -> str:
     loader = ""
     loaderVer = ""
     minecraftVer = ""
-    javaVer = ""
     for i, line in enumerate(file):
         if i < 700:
-            if re.match(NEOFORGE_LOADER_DETECT_REGEX,line):
+            if re.match(NEOFORGE_LOADER_DETECT_REGEX,line): #neoforge detection
                 loader = "NeoForge"
-                loaderVer = re.findall(NEOFORGE_LOADER_VERSION_EXTRACT_REGEX,line)[0]
+                loaderVer = re.findall(NEOFORGE_LOADER_VERSION_EXTRACT_REGEX,line)[0] #neoforge version extration
                 print("Detected loader is Neoforge, version " + str(loaderVer))
 
-                minecraftVer = re.findall(NEOFORGE_MC_VERSION_EXTRACT_REGEX,loaderVer)[0]
+                minecraftVer = re.findall(NEOFORGE_MC_VERSION_EXTRACT_REGEX,loaderVer)[0] #mc version extraction
                 minecraftVer = "1." + minecraftVer
                 print("Minecraft version is " + str(minecraftVer))
+            if re.match(FABRIC_LOADER_DETECT_REGEX,line) and i == 0: #fabric loader detection
+                loader = "Fabric"
+                loaderVer = re.findall(FABRIC_LOADER_VERSION_EXTRACT_REGEX,line)[0]
+                print("Loader is Fabric version " + str(loaderVer))
 
+                minecraftVer = re.findall(FABRIC_MC_VERSION_EXTRACT_REGEX,line)[0]
+                print("Minecraft is version " + minecraftVer)
 
     gameVersions = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer}
     return gameVersions
