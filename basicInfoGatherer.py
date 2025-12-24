d7716f54dfff976cf076ad8d7387fd1130955efa
V3AI-Seven <127050193+MrSeven3@users.noreply.github.com>
2025-12-24 10:52:16 -0500

    Got fabric and quilt crash report detection and data extraction. Changed how files are loaded in main script slightly

--- basicInfoGatherer.py
+++ basicInfoGatherer.py
@@ -1,30 +1,63 @@
 import re
 from io import TextIOWrapper
 
-NEOFORGE_LOADER_DETECT_REGEX = r"\t{2}NeoForge.*"
+def isCrashReport(file:TextIOWrapper) -> bool: #checks if the provided file is a crash report or not
+    CRASH_REPORT_DETECT_REGEX = r"-+"
 
-NEOFORGE_LOADER_VERSION_EXTRACT_REGEX = r"\t{2}NeoForge ([0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,4}).*"
-NEOFORGE_MC_VERSION_EXTRACT_REGEX = r"([0-9]{1,2}\.[0-9]{1,3})\.[0-9]{1,4}" #extracts the minecraft version from the provided neoforge version
+    for i, line in enumerate(file):
+        if i == 0 and re.match(CRASH_REPORT_DETECT_REGEX,line):
+            return True
+        else:
+            return False
+    return None
+
+def crashReportBasicInfoGatherer(file:TextIOWrapper):
+    QUILT_LOADER_DETECT_REGEX = r"---- Quilt Loader: .* ----"
+    QUILT_LOADER_VERSION_EXTRACT_REGEX = r"\| Quilt Loader\s*\| quilt_loader\s*\| ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})\s*.*"
+    QUILT_MC_VERSION_EXTRACT_REGEX = r"\| Minecraft\s*\| minecraft\s*\| ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})\s*.*"
+
+    FABRIC_LOADER_DETECT_REGEX = r"\sfabricloader: Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"
+    FABRIC_LOADER_VERSION_EXTRACT_REGEX = r"\sfabricloader: Fabric Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
+    FABRIC_MC_VERSION_EXTRACT_REGEX = r"\sminecraft: Minecraft ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
 
 
-FABRIC_LOADER_DETECT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+    loader = ""
+    loaderVer = ""
+    minecraftVer = ""
+    for i,line in enumerate(file):
+        if i == 0 and re.match(QUILT_LOADER_DETECT_REGEX, line):
+            loader = "Quilt"
+        if loader == "Quilt":
+            if re.match(QUILT_MC_VERSION_EXTRACT_REGEX,line):
+                minecraftVer = re.findall(QUILT_MC_VERSION_EXTRACT_REGEX,line)[0]
+            if re.match(QUILT_LOADER_VERSION_EXTRACT_REGEX,line):
+                loaderVer = re.findall(QUILT_LOADER_VERSION_EXTRACT_REGEX,line)[0]
 
-FABRIC_LOADER_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*"
-FABRIC_MC_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft ([0-9]\.[0-9]{1,2}\.[0-9]{1,2}) with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+        if re.match(FABRIC_LOADER_DETECT_REGEX,line):
+            loader = "Fabric"
+            loaderVer = re.findall(FABRIC_LOADER_VERSION_EXTRACT_REGEX,line)[0]
 
 
-QUILT_LOADER_DETECT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Quilt Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+    gameVersions = {"loader": loader, "loaderVersion": loaderVer, "minecraftVersion": minecraftVer,"isCrashReport":True}
+    return gameVersions
 
-QUILT_LOADER_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Quilt Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*"
-QUILT_MC_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft ([0-9]\.[0-9]{1,2}\.[0-9]{1,2}) with Quilt Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+def checkGameVersions(file:TextIOWrapper): #gets all basic versions from a log file, including modloader, modloader version, and minecraft version
+    NEOFORGE_LOADER_DETECT_REGEX = r"\t{2}NeoForge.*"
+    NEOFORGE_LOADER_VERSION_EXTRACT_REGEX = r"\t{2}NeoForge ([0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,4}).*"
+    NEOFORGE_MC_VERSION_EXTRACT_REGEX = r"([0-9]{1,2}\.[0-9]{1,3})\.[0-9]{1,4}"  # extracts the minecraft version from the provided neoforge version
 
+    FABRIC_LOADER_DETECT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+    FABRIC_LOADER_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Fabric Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*"
+    FABRIC_MC_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft ([0-9]\.[0-9]{1,2}\.[0-9]{1,2}) with Fabric Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
 
-FORGE_LOADER_DETECT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.forgeVersion, [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"
+    QUILT_LOADER_DETECT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Quilt Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
+    QUILT_LOADER_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft [0-9]\.[0-9]{1,2}\.[0-9]{1,2} with Quilt Loader ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}).*"
+    QUILT_MC_VERSION_EXTRACT_REGEX = r".* \[main\/INFO\]: Loading Minecraft ([0-9]\.[0-9]{1,2}\.[0-9]{1,2}) with Quilt Loader [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}.*"
 
-FORGE_LOADER_VERSION_EXTRACT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.forgeVersion, ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
-FORGE_MC_VERSION_EXTRACT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.mcVersion, ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
+    FORGE_LOADER_DETECT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.forgeVersion, [0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"
+    FORGE_LOADER_VERSION_EXTRACT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.forgeVersion, ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
+    FORGE_MC_VERSION_EXTRACT_REGEX = r".*\[main\/INFO\].*: ModLauncher running.* --fml.mcVersion, ([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})"
 
-def checkGameVersions(file:TextIOWrapper) -> str:
     loader = ""
     loaderVer = ""
     minecraftVer = ""
@@ -33,39 +66,42 @@ def checkGameVersions(file:TextIOWrapper) -> str:
             if re.match(NEOFORGE_LOADER_DETECT_REGEX,line): #neoforge detection
                 loader = "NeoForge"
                 loaderVer = re.findall(NEOFORGE_LOADER_VERSION_EXTRACT_REGEX,line)[0] #neoforge version extration
-                print("Detected loader is Neoforge, version " + str(loaderVer))
+                #print("Detected loader is Neoforge, version " + str(loaderVer))
 
                 minecraftVer = re.findall(NEOFORGE_MC_VERSION_EXTRACT_REGEX,loaderVer)[0] #mc version extraction
                 minecraftVer = "1." + minecraftVer
-                print("Minecraft version is " + str(minecraftVer))
+                #print("Minecraft version is " + str(minecraftVer))
                 break
 
             elif re.match(FABRIC_LOADER_DETECT_REGEX,line) and i == 0: #fabric loader detection
                 loader = "Fabric"
                 loaderVer = re.findall(FABRIC_LOADER_VERSION_EXTRACT_REGEX,line)[0]
-                print("Loader is Fabric version " + str(loaderVer))
+                #print("Loader is Fabric version " + str(loaderVer))
 
                 minecraftVer = re.findall(FABRIC_MC_VERSION_EXTRACT_REGEX,line)[0]
-                print("Minecraft is version " + str(minecraftVer))
+                #print("Minecraft is version " + str(minecraftVer))
                 break
 
             elif re.match(QUILT_LOADER_DETECT_REGEX,line) and i == 0: #quilt loader detection
                 loader = "Quilt"
                 loaderVer = re.findall(QUILT_LOADER_VERSION_EXTRACT_REGEX,line)[0]
-                print("Loader is Quilt version " + str(loaderVer))
+                #print("Loader is Quilt version " + str(loaderVer))
 
                 minecraftVer = re.findall(QUILT_MC_VERSION_EXTRACT_REGEX,line)[0]
-                print("Minecraft is version " + str(minecraftVer))
+                #print("Minecraft is version " + str(minecraftVer))
                 break
 
             elif re.match(FORGE_LOADER_DETECT_REGEX,line) and i == 0: #forge loader detection TODO: make this reliable, it may not always detect forge
                 loader = "Forge"
                 loaderVer = re.findall(FORGE_LOADER_VERSION_EXTRACT_REGEX,line)[0]
-                print("Loader is Forge version " + str(loaderVer))
+                #print("Loader is Forge version " + str(loaderVer))
 
                 minecraftVer = re.findall(FORGE_MC_VERSION_EXTRACT_REGEX,line)[0]
-                print("Minecraft is version " + str(minecraftVer))
+                #print("Minecraft is version " + str(minecraftVer))
 
-    gameVersions = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer}
+    if loader == '':
+        print("It appears you are playing vanilla, or you are using an unsupported modlauncher. Detection and analysis will continue as though your game is vanilla, but it may not be correct.")
+        loader = "Vanilla"
+    gameVersions = {"loader":loader, "loaderVersion":loaderVer, "minecraftVersion":minecraftVer,"isCrashReport":False}
     return gameVersions
 
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
+import os.path
+import sys
 import re
 import basicInfoGatherer
 import time
 
-
-LOG_EXTENSION_REGEX = r".+\.log|txt"
+LOG_EXTENSION_REGEX = r".*\.log|.*\.txt"
 
 print("Welcome to MCLOGS, the Minecraft log analyzer! (yes, that stands for something)")
 print("Please enter the path of the log to analyze. In the future, this can be done use a command line argument")
 file_path = input()
 
+if not os.path.exists(file_path):
+    print("We couldn't find a file at that path")
+    print("Please enter a path that is valid")
+    time.sleep(3)
+    sys.exit(1)
+
+
 if not re.match(LOG_EXTENSION_REGEX, file_path):
     print("Please enter a file that has the extentions .log or .txt, as they are the only ones that are valid Minecraft log files")
+    time.sleep(3)
+    sys.exit(1)
 else:
     file = open(file_path, "r",errors='ignore')
-    loader = basicInfoGatherer.checkGameVersions(file)
+    data = []
+    isCrashReport = False
+    if basicInfoGatherer.isCrashReport(file):
+        isCrashReport = True
+
+        file.close()
+        file = open(file_path, "r", errors='ignore')
+
+        data = basicInfoGatherer.crashReportBasicInfoGatherer(file)
+        print("Gathered basic data:")
+        print(str(data))
+    else:
+
+        file.close()
+        file = open(file_path, "r", errors='ignore')
+
+        data = basicInfoGatherer.checkGameVersions(file)
+        print("Gathered basic data:")
+        print(str(data))
